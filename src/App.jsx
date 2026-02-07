3c69e47f119e279a7cbfd5c67ab40f74bad679c1
athmika2904 <athmika.cs24@bmsce.ac.in>
2026-02-07 14:06:53 +0530

    set up app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react';
+import SubjectForm from './assets/Subjectform';
+import Subjectcard from './assets/Subjectcard';
 import './App.css'
 
 function App() {
+  const [subject,setubject]=useState([]);
+  const addSubject=(name)=>{
+    const newSubject={
+      id:Date.now(),
+      name,
+      attended:0,
+      total:0
+    }
+    setubject([...subject,newSubject]);
+  }
+  const MarkPresent=(id)=>{
+    setubject(subject.map((sub)=>{
+      sub.id===id?{...sub,attended:sub.attended+1,total:sub.total+1}:sub
+    }));
+  }
+  const MarkAbsent=(id)=>{
+    setubject(subject.map((sub)=>{
+      sub.id===id?{...sub,total:sub.total+1}:sub
+    }));
+  }
+  const DeleteSubject=(id)=>{
+    setubject(subject.filter((sub)=>{
+      sub.id!==id;
+    }));
+  }
   return (
-    <>
+    <div className='Plannercard'>
       <h1>Attendance Planner</h1>
-    </>
-  )
+      <SubjectForm addSubject={addSubject}/>
+      {subject.map((sub)=>(
+        <Subjectcard key={sub.id} subject={sub} MarkPresent={MarkPresent} MarkAbsent={MarkAbsent} DeleteSubject={DeleteSubject}/>
+      ))}
+    </div>
+  );
 }
 
 export default App
